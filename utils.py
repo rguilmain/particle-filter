6a6e5741666ae2d266c942ba59275cb41f570ea7
Nabin Sharma <nabin.s.sharma@gmail.com>
2014-05-05 22:48:31 -0400

    Add a utility to draw fov boarder

--- utils.py
+++ utils.py
@@ -3,6 +3,12 @@
 
 """Utilities
 """
+import numpy
+
+import matplotlib
+
+import trig
+
 
 # Convert a dictionary to a class
 # Reference: http://kiennt.com/blog/2012/06/14/python-object-and-dictionary-convertion.html
@@ -20,3 +26,32 @@ class Struct(object):
         self.__dict__[k] = []
         for e in v:
           self.__dict__[k].append(Struct(e))
+
+
+def extract_position_from_particles(particle_xs, particle_ys, weights):
+  assert(len(particle_xs) == len(particle_ys) == len(weights))
+  x = 0.0
+  y = 0.0
+  for px, py in zip(particle_xs, particle_ys):
+    x += px
+    y += py
+  return x / len(particle_xs), y / len(particle_xs)
+
+
+def draw_fov(ax):
+  """Don't mind the magic numbers for now.
+  """
+  p_left = 500.0 * numpy.array(
+    [trig.sind(-45.0), trig.cosd(45.0)])
+  p_right = 500.0 * numpy.array(
+    [trig.cosd(45.0), trig.sind(45.0)])
+  line_left = matplotlib.lines.Line2D(
+    [0, p_left[0]], [0, p_left[1]], color='b')
+  line_right = matplotlib.lines.Line2D(
+    [0, p_right[0]], [0, p_right[1]], color='b')
+  arc = matplotlib.patches.Arc((0, 0), 2*500, 2*500,
+                               theta1=45, theta2=135, color='b')
+  ax.add_line(line_left)
+  ax.add_line(line_right)
+  ax.add_patch(arc)
+  
